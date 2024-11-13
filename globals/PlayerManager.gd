95ffada2e4a08527d8c40119ed793a397df30695
Brian Hall <30417425+brianhall1313@users.noreply.github.com>
2024-11-12 21:39:58 -0800

    reworked box population, started blanking out player

---
+++ globals/PlayerManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var player_name:String = "Default"
+var player_max_health:int = 100
+var player_current_health:int = player_max_health
+
+func load_player(slot:int)->void:
+	pass
--- scenes/box.gd
+++ scenes/box.gd
@@ -1,42 +1,11 @@
 extends Control
 
-@onready var die_1: TextureRect = $dice_grid/GridContainer/die
-@onready var die_2: TextureRect = $dice_grid/GridContainer/die2
-@onready var die_3: TextureRect = $dice_grid/GridContainer/die3
-@onready var die_4: TextureRect = $dice_grid/GridContainer/die4
-@onready var die_5: TextureRect = $dice_grid/GridContainer/die5
-@onready var die_6: TextureRect = $dice_grid/GridContainer/die6
-@onready var die_7: TextureRect = $dice_grid/GridContainer/die7
-@onready var die_8: TextureRect = $dice_grid/GridContainer/die8
-@onready var die_9: TextureRect = $dice_grid/GridContainer/die9
-@onready var die_10: TextureRect = $dice_grid/GridContainer/die10
-@onready var die_11: TextureRect = $dice_grid/GridContainer/die11
-@onready var die_12: TextureRect = $dice_grid/GridContainer/die12
 
-
-@onready var grid:Array[Array] = [
-		[
-			die_1,
-			die_2,
-			die_3,
-			die_4
-		],
-		[
-			die_5,
-			die_6,
-			die_7,
-			die_8
-		],
-		[
-			die_9,
-			die_10,
-			die_11,
-			die_12
-		]
-	]
+@onready var dice_grid: GridContainer = $dice_grid_margin/dice_grid
+@onready var die:PackedScene = preload("res://scenes/die.tscn")
 
 var box_inventory:Array = []
-
+var box_area:int=12
 
 func _ready() -> void:
 	update_grid()
@@ -45,13 +14,14 @@ func _ready() -> void:
 func update_grid() -> void:
 	box_inventory = InventoryManager.inventory.duplicate(true)
 	box_inventory.shuffle()
-	var i = 0
-	for x in len(grid):
-		for y in len(grid[x]):
-			if i < len(box_inventory):
-				grid[x][y].setup(box_inventory[i]["name"],box_inventory[i]["type"],box_inventory[i]["faces"])
-				grid[x][y].roll()
-				i += 1
-			else:
-				grid[x][y].set_blank()
+	for item in box_inventory:
+		if dice_grid.get_child_count()<12:
+			var new = die.instantiate()
+			dice_grid.add_child(new)
+			new.setup(item["name"],item["type"],item["faces"])
+			new.roll()
+	while dice_grid.get_child_count() < 12:
+		var new = die.instantiate()
+		dice_grid.add_child(new)
+		new.set_blank()
 			
