87af629fc583c3fad4287254fe7560f33e5a747c
Brian Hall <30417425+brianhall1313@users.noreply.github.com>
2024-11-12 20:54:03 -0800

    displaying the player inventory

--- scenes/dice_entry.gd
+++ scenes/dice_entry.gd
@@ -1,11 +1,22 @@
 extends VBoxContainer
 
 @onready var full_dice_view: VBoxContainer = $full_dice_view
+@onready var type_icon: TextureRect = $dice_summary/type_icon
+@onready var glyph_title: Label = $dice_summary/glyph_title
+@onready var count: Label = $dice_summary/count
+@onready var glyph_container: HBoxContainer = $full_dice_view/glyph_container
 
 
 func _gui_input(event: InputEvent) -> void:
 	if event.is_action_released("click"):
 		full_dice_view.visible = !full_dice_view.visible
 
-func setup() -> void:
-	pass
+func setup(dice:Dictionary) -> void:
+	type_icon.texture = Global.glyph_list[dice["type"]][""]
+	glyph_title.text = dice["name"]
+	count.text = str(dice["count"])
+	var list:Array = glyph_container.get_children()
+	var i = 0
+	for item in list:
+		item.texture = Global.glyph_list[dice["type"]][dice["faces"][i]]
+		i += 1
--- scenes/dice_inventory.gd
+++ scenes/dice_inventory.gd
@@ -1,5 +1,9 @@
 extends ScrollContainer
 
+
+@onready var dice_list: VBoxContainer = $dice_list
+@onready var dice_entry:PackedScene=preload("res://scenes/dice_entry.tscn")
+
 var display_inventory:Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +18,8 @@ func update_display() -> void:
 	for item in display_inventory:
 		item["count"]=InventoryManager.inventory.count(item)
 	#TODO the rest of this nonsense
+	for item in display_inventory:
+		var new = dice_entry.instantiate()
+		dice_list.add_child(new)
+		new.setup(item)
+		
