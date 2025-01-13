af5dc5af6d8e3ee17cbe207a0c9c6f45a06af76b
Brian Hall <30417425+brianhall1313@users.noreply.github.com>
2025-01-12 18:48:31 -0800

    damage numbers now have a color based on damage type

--- resources/damage_number_label.gd
+++ resources/damage_number_label.gd
@@ -1,15 +1,35 @@
 extends Label
 
+var element_color:String="white"
+
 func display(damage:Damage) -> void:
-	modulate = Color("white",0.0)
+	element_color = get_color(damage)
+	modulate = Color(element_color,0.0)
 	text = str(damage.damage)
 	play()
 
 func play() -> void:
 	var tween = create_tween()
-	tween.tween_property(self,"modulate",Color("white",1.0),.2)
+	tween.tween_property(self,"modulate",Color(element_color,1.0),.2)
 	await get_tree().create_timer(1.0).timeout
 	tween = create_tween()
-	tween.tween_property(self,"modulate",Color("white",0.0),1)
+	tween.tween_property(self,"modulate",Color(element_color,0.0),1)
 	await tween.finished
 	queue_free()
+
+func get_color(damage:Damage) -> String:
+	match damage.type:
+		"fire":
+			return "orange"
+		"water":
+			return "blue"
+		"earth":
+			return "brown"
+		"sky":
+			return "yellow"
+		"bleed":
+			return "red"
+		"poison":
+			return "green"
+		_:
+			return "white"
