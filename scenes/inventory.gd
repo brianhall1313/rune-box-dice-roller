d7ccf5a3e197278dc9c087fb65f0b10d303ce8b5
Brian Hall <30417425+brianhall1313@users.noreply.github.com>
2025-02-14 18:32:32 -0800

    inventory now confirms if you want to change the dice inventory

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var inventory_container: GridContainer = $main/inventory/inventory_margin/inventory_display/inventory_scroll/inventory_container
 @onready var dice_entry: PackedScene = preload("res://resources/inventory_dice_entry.tscn")
+@onready var popup: PanelContainer = $popup
+@onready var dni_panel: Panel = $"DNI panel"
 
 var display_inventory:Array[Dictionary] = []
 
@@ -39,11 +41,30 @@ func attach_signals() -> void:
 
 
 func _on_exit_button_button_up() -> void:
-	InventoryManager.build_inventory(display_inventory)
-	get_tree().change_scene_to_file("res://scenes/overworld.tscn")
+	dni_panel.show()
+	popup.show()
 
 func _delete_item_at(index) -> void:
 	display_inventory.remove_at(index)
 	#print(display_inventory)
 	update_inventory()
 	
+
+
+func _on_undo_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/overworld.tscn")
+
+
+func _on_confirm_button_up() -> void:
+	#update inventory state
+	InventoryManager.build_inventory(display_inventory)
+	#inventory and player state get saved together
+	PlayerManager.save_player()
+	#go to world map
+	get_tree().change_scene_to_file("res://scenes/overworld.tscn")
+	
+
+
+func _on_go_back_button_up() -> void:
+	dni_panel.hide()
+	popup.hide()
